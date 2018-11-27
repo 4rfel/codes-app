0f820501cbe22568cc315de47a964d3c80b9eb36
4rfel <38050598+4rfel@users.noreply.github.com>
2018-11-27 06:55:51 -0200

    aa

--- assets_app/scripts/dropdown.js
+++ assets_app/scripts/dropdown.js
@@ -1,15 +1,23 @@
-var i = 1;
-function evento()
-{
-    i += 1;
-    console.log(i);
-    var questao = document.querySelector('.souburro');
-    questao.innerHTML = "Questão número: " + i;
-
-}
-
 $(document).ready(function ()
 {
+    // Carrega quiz
+    var num_pergunta = 0;
+    var pontos = 0;
+    var file = "assets_app/quiz.txt";
+    let db = new DB('https://desgosto-final.firebaseio.com/')
+
+    db.download("/", function(data) {
+        // Printing the data contained on that path on firebase
+        console.log(data)
+
+        perguntas = data.perguntas;
+        $('.pergunta').html(data.perguntas[num_pergunta].pergunta);
+        $('.resp1').html(data.perguntas[num_pergunta].respostas[0]);
+        $('.resp2').html(data.perguntas[num_pergunta].respostas[1]);
+        $('.resp3').html(data.perguntas[num_pergunta].respostas[2]);
+        $('.resp4').html(data.perguntas[num_pergunta].respostas[3]);
+    });
+
     $('.sidebarCollapse').click(function ()
     {
         $('.sidebar').toggleClass('active');
@@ -27,6 +35,19 @@ $(document).ready(function ()
 
     $('#submit_quiz').click(function ()
     {
+        // Verifica resposta correta
+        var resposta_correta = perguntas[num_pergunta].resposta_correta;
+        var answer = $(document).find('.selected').data('id');
+        if (resposta_correta === answer)
+        {
+            pontos += 1;
+            $('.pontos').html("Pontos: " + pontos);
+        }
+
+        // Incrementa pergunta
+        num_pergunta += 1;
+        $('.souburro').html("Questão número: " + (num_pergunta + 1));
+
         // Desseleciona a resposta selecionada
         var elements = $('.click');
         elements.each(function ()
@@ -34,7 +55,11 @@ $(document).ready(function ()
             $(this).removeClass('selected');
         });
 
-        // Faz alguma coisa quando clica no botão enviar
+        $('.pergunta').html(perguntas[num_pergunta].pergunta);
+        $('.resp1').html(perguntas[num_pergunta].respostas[0]);
+        $('.resp2').html(perguntas[num_pergunta].respostas[1]);
+        $('.resp3').html(perguntas[num_pergunta].respostas[2]);
+        $('.resp4').html(perguntas[num_pergunta].respostas[3]);
     });
 
     $('.dropdown-trigger').dropdown({
